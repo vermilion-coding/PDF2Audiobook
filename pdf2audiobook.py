9715a7474b533098fffe3ea7d9588e07979c049e
vermilion-coding <138716457+vermilion-coding@users.noreply.github.com>
2023-07-10 11:38:33 -0400

    Add files via upload

---
+++ pdf2audiobook.py
@@ -0,0 +1,42 @@
+import pyttsx3
+import PyPDF2
+import PySimpleGUI as sg
+from gtts import gTTS
+import pathlib
+
+def shorten_path(filepath):
+    path = pathlib.Path(filepath)
+    return path.stem
+
+def convert_to_audio(filename, location):
+    book = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfReader(book)
+    pages = len(pdfReader.pages)
+
+    txtlist = []
+    for i in range(pages):
+        try:
+            page = pdfReader.pages[i]
+            txtlist.append(page.extract_text())
+        except:
+            pass
+    textstring = ' '.join(txtlist)
+    language = 'en'
+    myAudio = gTTS(text=textstring, lang=language, slow=False)
+    myAudio.save(location + "/" + shorten_path(filename) + ".mp3")
+
+layout = [
+    [sg.Text("Input File:"), sg.Input(key='-IN-'), sg.FileBrowse(file_types=(("PDF Files", "*.pdf"),))],
+    [sg.Text("Download Location:"), sg.Input(key='-IN2-'), sg.FolderBrowse()],
+    [sg.Exit(), sg.Button("Play Audiobook")]
+]
+
+window = sg.Window("PDF Text to Speech", layout)
+
+while True:
+    event, values = window.read()
+    if event in (sg.WINDOW_CLOSED, "Exit"):
+        break
+    if event == "Play Audiobook":
+        convert_to_audio(filename=values["-IN-"], location=values["-IN2-"])
+window.close()
